65d7b0554031c918f3975b9c5677654a0e0d53d1
Aaron Tull <atull@onetechnologies.net>
2013-05-30 01:35:11 -0500

    First

---
+++ canopy.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Canopy (run) where
+
+import Data.Text (Text, splitOn, append, toUpper, toLower)
+import Test.WebDriver 
+
+capabilities = defaultCaps { browser = firefox }
+
+run = runSession defaultSession capabilities $ do 
+	url "http://stackoverflow.com" 
+	e1 <- find "#nav-questions"
+	click e1
+
+url = openPage
+
+find selector = findElem $ ByCSS selector 
---
+++ main.hs
@@ -0,0 +1,6 @@
+import qualified Canopy as C
+
+main = do 
+	title <- C.run
+	print title
+	getChar
